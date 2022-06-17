80dac49744ff5a6de27c79a90d6dc36cc14c7a6b
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-06-17 18:48:52 +0430

    1

---
+++ main/ir/ac/kntu/Main.java
@@ -0,0 +1,11 @@
+package main.ir.ac.kntu;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+    }
+}
