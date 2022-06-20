1481fbc574fd8a541190b4659547816a61d61fd0
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-06-20 10:38:21 +0430

    1

--- main/ir/ac/kntu/Main.java
+++ main/ir/ac/kntu/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
+        if(n % 2 != 0){
+            System.out.println(":(");
+        } else{
 
+        }
     }
 }
