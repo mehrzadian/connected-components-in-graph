cbcf5b3d522ed36bd5c040899a1c1be9c290fa21
mehrzadian <amirreza.313.mehrzadian@gmail.com>
2022-06-27 20:56:38 +0430

    2

--- main/ir/ac/kntu/Graph.java
+++ main/ir/ac/kntu/Graph.java
@@ -1,84 +1,121 @@
 package main.ir.ac.kntu;
 
-
-// Java program to print connected components in
-// an undirected graph
 import java.util.ArrayList;
-public class Graph
-{
-    // A user define class to represent a graph.
-    // A graph is an array of adjacency lists.
-    // Size of array will be V (number of vertices
-    // in graph)
-    int V;
-    ArrayList<ArrayList<Integer> > adjListArray;
+import java.util.Scanner;
+import java.util.Vector;
 
-    // constructor
-    Graph(int V)
-    {
-        this.V = V;
-        // define the size of array as
-        // number of vertices
-        adjListArray = new ArrayList<>();
+public class Graph {
+    // graph
+    static ArrayList<ArrayList<Integer>> g = new ArrayList<ArrayList<Integer>>();
 
-        // Create a new list for each vertex
-        // such that adjacent nodes can be stored
+    static int res;
 
-        for (int i = 0; i < V; i++) {
-            adjListArray.add(i, new ArrayList<>());
-        }
+    public Graph() {
     }
 
-    // Adds an edge to an undirected graph
-    void addEdge(int src, int dest)
-    {
-        // Add an edge from src to dest.
-        adjListArray.get(src).add(dest);
-
-        // Since graph is undirected, add an edge from dest
-        // to src also
-        adjListArray.get(dest).add(src);
+    public ArrayList<ArrayList<Integer>> getG() {
+        return g;
     }
 
-    void DFSUtil(int v, boolean[] visited)
-    {
-        // Mark the current node as visited and print it
-        visited[v] = true;
-        System.out.print(v + " ");
-        // Recur for all the vertices
-        // adjacent to this vertex
-        for (int x : adjListArray.get(v)) {
-            if (!visited[x])
-                DFSUtil(x, visited);
-        }
+    public void setG(ArrayList<ArrayList<Integer>> g) {
+        this.g = g;
     }
-    void connectedComponents()
+
+    public int getRes() {
+        return res;
+    }
+
+    public void setRes(int res) {
+        this.res = res;
+    }
+
+    // Utility method to do DFS of the graph and count edge
+// deletion for even forest
+    private static int dfs( int u, boolean visit[])
     {
-        // Mark all the vertices as not visited
-        boolean[] visited = new boolean[V];
-        for (int v = 0; v < V; ++v) {
-            if (!visited[v]) {
-                // print all reachable vertices
-                // from v
-                DFSUtil(v, visited);
-                System.out.println();
+        visit[u] = true;
+        int currComponentNode = 0;
+
+        // iterate over all neighbor of node u
+        for (int i = 0; i < g.get(u).size(); i++)
+        {
+            int v = g.get(u).get(i);
+            if (!visit[v])
+            {
+                // Count the number of nodes in a subtree
+                int subtreeNodeCount = dfs(v, visit);
+
+                // if returned node count is even, disconnect
+                // the subtree and increase result by one.
+                if (subtreeNodeCount % 2 == 0)
+                    res++;
+
+                    // else add subtree nodes in current component
+                else
+                    currComponentNode += subtreeNodeCount;
             }
         }
+
+        // number of nodes in current component and one for
+        // current node
+        return (currComponentNode + 1);
     }
 
-    // Driver code
-    public static void main(String[] args)
+    /* method returns max edge that we can remove, after which
+        each connected component will have even number of
+        vertices */
+    private static int maxEdgeRemovalToMakeForestEven( int N)
     {
-        // Create a graph given in the above diagram
-        Graph g = new Graph(
-                5); // 5 vertices numbered from 0 to 4
+        // Create a visited array for DFS and make all nodes
+        // unvisited in starting
+        boolean visit[]=new boolean[N + 1];
+        for (int i = 0; i <= N; i++)
+            visit[i] = false;
 
-        g.addEdge(1, 0);
-        g.addEdge(2, 3);
-        g.addEdge(3, 4);
-        System.out.println(
-                "Following are connected components");
-        g.connectedComponents();
+        res = 0; // Passed as reference
+
+        // calling the dfs from node-0
+        dfs(0, visit);
+
+        return res;
     }
-}
 
+    // Utility function to add an undirected edge (u,v)
+    private static void addEdge( int u, int v)
+    {
+        g.get(u).add(v);
+        g.get(v).add(u);
+    }
+
+    private static void makeGraph(int[][] edges, int N) {
+        for (int i = 0; i < N +1; i++)
+            g.add(new ArrayList<Integer>());
+
+        for (int i = 0; i < N; i++)
+            addEdge( edges[i][0], edges[i][1]);
+    }
+
+    // Driver code to test above methods
+    public static void main(String args[]) {
+
+
+        int edges[][] ={{0, 2}, {0, 1}, {0, 4},
+                {2, 3}, {4, 5}, {5, 6},
+                {5, 7}};
+        int N = edges.length;
+        makeGraph(edges, N);
+        System.out.println(maxEdgeRemovalToMakeForestEven( N));
+        int edges3[][]={{7},{5,10},{5,9},
+                {7,8},{2,3,6},{5},
+                {1,4},{4,10},{3},
+                {2,8}};
+        int N3 = edges3.length;
+        makeGraph(edges3,N3);
+        System.out.println(maxEdgeRemovalToMakeForestEven( N3));
+        int edges2[][]={{2},{1,3},{2}};
+        int N2 = edges2.length;
+        makeGraph(edges2,N2);
+        System.out.println(maxEdgeRemovalToMakeForestEven( N2));
+    }
+
+}
--- main/ir/ac/kntu/Main.java
+++ main/ir/ac/kntu/Main.java
@@ -1,15 +1,10 @@
 package main.ir.ac.kntu;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        if(n % 2 != 0){
-            System.out.println(":(");
-        } else{
 
-        }
     }
 }
